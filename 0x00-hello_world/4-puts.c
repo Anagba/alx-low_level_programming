cbc95c3a7b9dd3d6f71587738d6ab230f0e0527f
Anagba <urielanagba@gmail.com>
2023-02-16 15:49:50 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * description: 'to print essence of programming'
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-puts("Programming is like building a multilinginual puzzle");
+puts(""Programming is like building a multilinginual puzzle");
 return (0);
 }
