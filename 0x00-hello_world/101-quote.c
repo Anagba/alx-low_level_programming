26ec409214f30e43ffef10986226cf322ca3d099
Anagba <urielanagba@gmail.com>
2023-02-16 18:38:39 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
  *
  * Description: 'A program that prints exactly and that piece of art is useful'
  *
- * Return: Always 0 (success)
+ * Return:1
  */
+
 int main(void)
 {
-	write(2,"\ "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
-	return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015 - 10 - 19\n", 61);
+return (1);
 }
