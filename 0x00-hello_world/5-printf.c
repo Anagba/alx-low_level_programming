3ca38f10a97e891e747916a5a9dc717280b5ee10
Anagba <urielanagba@gmail.com>
2023-02-16 16:59:27 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: 'to print the beauty of grammar in art and a new line'
+ *
+ * Return: 0 (success)
+ */
 int main(void)
 {
-	/* main - Entry point
-	 *
-	/* Description: 'to use printf'
-	*
-	/* Return: Always 0 (success)
-	 */
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
