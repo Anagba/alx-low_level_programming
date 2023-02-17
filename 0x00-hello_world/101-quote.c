d200351584ff4cf37c5644696003e483c1ec465d
Anagba <urielanagba@gmail.com>
2023-02-16 18:03:17 -0800

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'A program that prints exactly and that piece of art is useful'
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+write("2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
+return (1);
+}
