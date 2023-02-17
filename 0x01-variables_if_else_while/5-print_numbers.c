cacf54a6943a3e87bdf6d97ce17d33c6b534fd01
Anagba <urielanagba@gmail.com>
2023-02-17 13:59:26 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'A program that prints all single digit numbers
+ * of base 10 starting from 0, followed by a new line'
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int a;
+
+for (a = 0; a < 10; a++)
+printf("%i", a);
+printf("\n");
+return (0);
+}
