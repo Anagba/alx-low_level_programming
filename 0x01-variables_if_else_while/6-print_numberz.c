a975998c55e3fa2f5aa3612713b8e697d17831ac
Anagba <urielanagba@gmail.com>
2023-02-17 14:08:36 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'A program that prints all single digit number of
+ * base 10 starting from 0 using putchar'
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int i = 28;
+
+while (i < 38)
+{
+putchar(i);
+i++;
+}
+putchar('\n');
+return (0);
+}
