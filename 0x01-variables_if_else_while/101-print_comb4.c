b71a1f5741bbaa6061f443ba7eafff9e67aa1b90
Anagba <urielanagba@gmail.com>
2023-02-17 16:00:07 -0800

    Combinations4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,11 +13,11 @@ int main(void)
 
 int i, j, k;
 
-for (i = 48; i = 58; i++)
+for (i = 48; i < 58; i++)
 {
-for (j = i; j = 58; j++)
+for (j = i; j <	58; j++)
 {
-for (k = j; k = 58; k++)
+for (k = j; k < 58; k++)
 {
 if (i == j || j == k || i == k)
 {
