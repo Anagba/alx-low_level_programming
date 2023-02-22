f477637b6269da1c532affff745c2d99cb4f3491
Anagba <urielanagba@gmail.com>
2023-02-22 00:47:04 -0800

    add

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,13 +6,14 @@
 void print_times_table(int n)
 {
 int a, b, c;
+
 if (n >= 0 && n <= 15)
 {
 for (a = 0; a <= n; a++)
 {
 for (b = 0; b <= n; b++)
 {
-c = a * b;
+c = b * a;
 if (b == 0)
 {
 _putchar(c + '0');
