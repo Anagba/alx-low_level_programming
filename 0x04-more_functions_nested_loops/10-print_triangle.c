03bf1e5fd7de4bbc7d36b1f91ea90f41ce009040
Anagba <urielanagba@gmail.com>
2023-02-23 16:40:09 -0800

    triangular

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_triangle -entry point
  *
@@ -6,26 +7,28 @@
  * @size: size of the triangle
  * Return: void
  */
+
 int print_triangle(int size)
 {
-int row, hashes, spaces;
+	int row, hashes, spaces;
 
-if (size <= 0)
-{
-_putchar('\n');
-}
-else
-{
-for (row = 1; row <= size; row++)
-{
-for (spaces = size - row; spaces >= 1; spaces--)
-{_putchar(' ');
-}
-for (hashes = 1; hashes <= row; hashes++)
-{
-_putchar('#');
-}
-_putchar('\n');
-}
-}
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (row = 1; row <= size; row++)
+		{
+			for (spaces = size - row; spaces >= 1; spaces--)
+			{
+				_putchar(' ');
+			}
+			for (hashes = 1; hashes <= row; hashes++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
 }
