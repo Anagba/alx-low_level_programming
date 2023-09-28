2257f1535e157bfaf0cf18886b5cf0f6e20b8459
Anagba <urielanagba@gmail.com>
2023-09-28 16:25:19 -0700

    updates

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - entry point
+ * @s: a character pointer
+ * Description: A program that prints out a string in reverse order
+ * Return: NULL
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+	_putchar('\n');
+}
