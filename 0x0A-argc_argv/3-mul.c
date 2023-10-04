9d14184297e7f464dbbbfd621bc01b61204ec0fb
Anagba <urielanagba@gmail.com>
2023-10-04 13:58:28 -0700

    performs multiplication of only to integers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,12 +13,16 @@ int main(int argc, char **argv)
 {
 	int  product;
 
-	if (argc > 3 || argc < 3)
+	if (argc < 3 || argc > 3)
+	{
 		printf("Error\n");
-	return (1);
+		return (1);
+	}
 
 	else
+	{
 		product	= atoi(*(argv + 1)) * atoi(*(argv + 2));
 		printf("%d\n", product);
+	}
 	return (0);
 }
