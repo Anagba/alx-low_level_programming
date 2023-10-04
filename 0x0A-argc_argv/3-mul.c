d0e95933e9a0c6f943f2af300d10b5047d3b43e5
Anagba <urielanagba@gmail.com>
2023-10-04 10:55:44 -0700

    performing multiplication

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: character pointer to argument passed into the command line
+ * Description: A program that multiplies two integers read
+ * Return: 0 if successful, if no argument is entered returns 1
+ */
+
+int main(int argc, char **argv)
+{
+	int p, product = 1;
+
+	if (argc > 1)
+	{
+		for (p = 0; p < argc; p++)
+		{
+			product *= atoi(argv[p]);
+		}
+		printf("%d\n", product);
+	}
+	if (argv[p] == 0)
+	{
+		printf("ERROR\n");
+		return (1);
+	}
+	return (0);
+}
