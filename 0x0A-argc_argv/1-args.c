d10760e648923a567b9fbad3e5d420295c3ece1b
Anagba <urielanagba@gmail.com>
2023-10-04 05:54:37 -0700

    Argument count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument used to store strings passed into the command line
+ * as arguments
+ * Description: A program that takes count of arguments passed
+ * Return: 0
+ */
+
+int main(int argc, char **argv[])
+{
+	int p;
+
+	if (argc > 1)
+		for (p = 1; p < argc; p++)
+			printf("%d\n", argv[p]);
+	else
+		printf("No argument passed\n");
+	return (0);
+}
