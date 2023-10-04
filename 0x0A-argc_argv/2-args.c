298628cd7074618b39c05affbfc5038fe8770950
Anagba <urielanagba@gmail.com>
2023-10-04 09:49:23 -0700

    new line added after every command

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: character pointer to every string passed into the
+ * command line
+ * Description: A program that prints all arguments received
+ * separating them with a new line
+ * Return: 0
+ */
+
+int main(int argc, char **argv[])
+{
+	int p;
+	if (argc > 1)
+		for (p = 1; p < argc; p++)
+		{
+			printf("%s", argv[p]);
+			printf("\n");
+		}
+	return (0);
+}
