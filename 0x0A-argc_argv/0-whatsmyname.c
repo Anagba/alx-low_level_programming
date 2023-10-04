1cfe4eb5a0490bcc9af075d4a8541f2f9a5b6e73
Anagba <urielanagba@gmail.com>
2023-10-04 05:39:36 -0700

    updates

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
 /**
  * main - entry point
  * @argc: takes count of the argument entered
@@ -13,8 +12,7 @@
 int main(int argc __attribute__((unused)), char **argv)
 {
 	
-	_putchar(argv);
-	_putchar('\n');
+	printf("%s\n", *argv);
 
 	return (0);
 }
